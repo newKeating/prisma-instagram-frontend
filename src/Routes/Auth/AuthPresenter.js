35d9acc3ab492b5deb051230f2dee493ba656783
Peter Kim <kroits09@gmail.com>
2019-05-08 17:26:09 +0900

    Separate Auth.js into Container/ Presenter/ Queries

--- src/Routes/Auth/AuthPresenter.js
+++ src/Routes/Auth/AuthPresenter.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import Input from "../Components/Input";
-import Button from "../Components/Button";
-import useInput from "../Hooks/useInput";
+import Input from "../../Components/Input";
+import Button from "../../Components/Button";
 
 const Wrapper = styled.div`
   min-height: 80vh;
@@ -47,25 +46,27 @@ const Form = styled(Box)`
   }
 `;
 
-export default () => {
-  const [action, setAction] = useState("logIn");
-  const username = useInput("");
-  const password = useInput("");
-  const firstName = useInput("");
-  const lastName = useInput("");
-  const email = useInput("");
-
+export default ({
+  action,
+  username,
+  password,
+  firstName,
+  lastName,
+  email,
+  setAction,
+  onLogin
+}) => {
   return (
     <Wrapper>
       <Form>
         {action === "logIn" ? (
-          <form>
+          <form onSubmit={onLogin}>
             <Input placeholder={"Username"} {...username} />
             <Input placeholder={"Password"} {...password} type="password" />
             <Button text={"Log in"} />
           </form>
         ) : (
-          <form>
+          <form onSubmit={onLogin}>
             <Input placeholder={"First name"} {...firstName} />
             <Input placeholder={"Last name"} {...lastName} />
             <Input placeholder={"Email"} {...email} type="email" />
